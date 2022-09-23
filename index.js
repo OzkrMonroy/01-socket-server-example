1a4e277e414f26e1219cbb2724d0145046304640
OscarMonroy <osmonroy@bi.com.gt>
2022-09-23 15:03:28 -0600

    Now the html is served by our server. The server can send info and the front can receive it.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-io.on('connection', () => {
-    console.log('Client is connected');
+
+app.use(express.static(__dirname + '/public'))
+io.on('connection', (socket) => {
+    console.log('Client is connected, with id:' + socket.id);
+    socket.emit('welcome-message', {
+        message: 'Welcome to the server',
+        date: new Date()
+    });
 });
 server.listen(8080, () => {
     console.log('Server running in port: 8080');
